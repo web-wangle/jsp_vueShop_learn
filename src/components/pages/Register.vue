a361e8e431c622d1e02f70718c9bb5f40decabfc
web-wangle <18829291736@163.com>
2019-08-14 16:46:05 +0800

    第30节完成

--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -5,7 +5,7 @@
     <van-field v-model="username" label="用户名" right-icon="clear" placeholder="请输入用户名" required @click-right-icon="username=''" />
     <van-field v-model="password" label="密码" placeholder="请输入密码" required />
     <div class="registerButton">
-      <van-button type="primary" size="large" @click="registerUser">马上注册</van-button>
+      <van-button type="primary" size="large" @click="registerAction" :loading="openLoading">马上注册</van-button>
     </div>
   </div>
 </template>
@@ -13,19 +13,27 @@
 <script>
 import axios from 'axios'
 import url from '@/serviceAPI.config.js'
+import { truncate } from 'fs';
 
 export default {
   data () {
     return {
       username: '',
       password: '',
+      openLoading: false,
+      usernameErrorMsg: '',
+      passwordErrorMsg: ''
     }
   },
   methods: {
-    goBack(){
+    goBack() {
       this.$router.go(-1);
     },
-    registerUser(){
+    registerAction() {
+      this.checkForm() && this.registerUser();
+    },
+    registerUser() {
+      this.openLoading = true;
       axios({
       url: url.registerUser,
       method: 'post',
@@ -35,6 +43,7 @@ export default {
         }
       })
       .then(res => {
+        this.openLoading = false;
         if(res.data.code === 200){
           this.$toast.success('注册成功');
         }else{
@@ -42,8 +51,25 @@ export default {
         }
       })
       .catch(err => {
+        this.openLoading = false;
         this.$toast.fail('注册失败');
       })
+    },
+    checkForm() {
+      let isOk = true;
+      if(this.username.length < 5){
+        this.usernameErrorMsg = '用户名不能小于5位';
+        isOk = false;
+      }else{
+        this.usernameErrorMsg = ''
+      }
+      if(this.password.length < 6){
+        this.passwordErrorMsg = '密码不能少于6位';
+        isOk = false;
+      }else{
+        this.passwordErrorMsg = '';
+      }
+      return isOk;
     }
   }
 }
